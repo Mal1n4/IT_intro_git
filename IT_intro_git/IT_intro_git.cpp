ff0d6ccf54a6b3c49a8e5747bda3b4108f0cefcc
avg111 <avg111@tpu.ru>
2023-12-14 09:52:22 +0700

    Список скриншотов для отчёта

--- IT_intro_git/IT_intro_git.cpp
+++ IT_intro_git/IT_intro_git.cpp
@@ -4,6 +4,6 @@ int main() {
     std::cout << "Hello World!\n";
     std::cout << "На базе\n"
     std::cout << "4 скриншота:\n";
-    std::cout <<"Список репозиториев\nДобавить RomanKovin в репозиторий+скрин(manage access)\n"
+    std::cout << "Список репозиториев\nДобавить RomanKovin в репозиторий+скрин(manage access)\nИзменения в репозитории напарника\nСкрин своих изменений в репозитории напарника на github\nВведение в ИТ.git2";
     return 0;
 }
